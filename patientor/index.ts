1b6234cdeb2cb0edcfe67419f76ace47beb3b7f2
Arslan <rslnkrmt2552@gmail.com>
2022-11-02 17:13:24 +0500

    ex 9.9

--- patientor/index.ts
+++ patientor/index.ts
@@ -6,7 +6,7 @@ app.get("/api/ping", (_req, resp) => {
   return resp.send("pong");
 });
 
-const PORT = 3003;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server up on http://localhost:${PORT}`);
 });
