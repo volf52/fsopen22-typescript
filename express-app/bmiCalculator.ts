5237c26deafbe1877f6a0bf40db97e1c3cfc4129
Arslan <rslnkrmt2552@gmail.com>
2022-11-02 15:39:29 +0500

    ex 9.1

---
+++ express-app/bmiCalculator.ts
@@ -0,0 +1,15 @@
+const calculateBmi = (heightInCm: number, weightInKg: number) => {
+  const heightInMeters = heightInCm / 100;
+
+  const bmi = weightInKg / (heightInMeters * heightInMeters);
+
+  if (bmi > 40) return "Obesity (Class 3)";
+  if (bmi > 35) return "Obesity (Class 2)";
+  if (bmi > 30) return "Obesity (Clas 1)";
+  if (bmi > 25) return "Overweight";
+  if (bmi > 18.5) return "Normal (healthy weight)";
+
+  return "Underweight";
+};
+
+console.log(calculateBmi(180, 74));
